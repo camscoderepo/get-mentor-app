2744c95f302fe2dff09a7eab74a88166f8d61920
Michael Haslam <ongomobile@gmail.com>
2020-04-28 12:14:04 +0700

    Refactor
    Added reusable button component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -108,7 +108,7 @@ const NavigationNonAuth = () => {
       </MobileLogoWrapper>
       <MobileSignInWrapper>
         <Link style={logInStyle} to={ROUTES.SIGN_IN}>
-          Log In
+          Log Init
         </Link>
       </MobileSignInWrapper>
       <Header>

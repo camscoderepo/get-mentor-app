a654985eb63e96bc8bc07542ae222495938fa0c8
Michael Haslam <ongomobile@gmail.com>
2020-05-05 10:14:13 +0700

    Refactor
    Second round fixing  issue of email sign up & sign in redirect to varification email

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -42,7 +42,7 @@ class Firebase {
     this.auth.currentUser.sendEmailVerification({
       url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
       url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT_ONE,
-      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT_LOCAL,
+      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT_TWO,
     });
 
   doPasswordUpdate = (password) =>

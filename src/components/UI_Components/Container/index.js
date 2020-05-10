f03e0eae2607b95ad1658a92b5060f5f93ee7b08
Michael Haslam <ongomobile@gmail.com>
2020-05-10 14:27:06 +0700

    Style
    Add Styles to container for multible screens

--- src/components/UI_Components/Container/index.js
+++ src/components/UI_Components/Container/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import media from '../Media';
 
 export const Container = styled.div`
-  background-color: ${(props) => props.setBgColor || 'red'};
+  background-color: ${(props) => props.setBgColor || 'transparent'};
   max-width: ${(props) => props.setWidth || '1440px'};
   margin-top: ${(props) => props.setMarTop || '8%'};
   padding-left: ${(props) => props.setPadLeft || '4.2%'};

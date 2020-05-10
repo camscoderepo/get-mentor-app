2c1f156df0db4bd7fbe6177686cab19395bd5520
Michael Haslam <ongomobile@gmail.com>
2020-05-10 14:25:08 +0700

    Style
    Add Styles to container for multible screens

--- src/components/UI_Components/Container/index.js
+++ src/components/UI_Components/Container/index.js
@@ -2,17 +2,33 @@ import styled from 'styled-components';
 import media from '../Media';
 
 export const Container = styled.div`
-  background-color: ${(props) => props.setBgColor || 'transparent'};
+  background-color: ${(props) => props.setBgColor || 'red'};
   max-width: ${(props) => props.setWidth || '1440px'};
   margin-top: ${(props) => props.setMarTop || '8%'};
   padding-left: ${(props) => props.setPadLeft || '4.2%'};
   padding-right: ${(props) => props.setPadRight || '4.2%'};
+ 
+  ${media.lgDesktop`
+     margin-left:auto;
+     margin-right:auto;
+     max-width:1920px;
+  `}
 
+  ${media.mdDesktop`
+     margin-left:auto;
+     margin-right:auto;
+  `}
+  
+  ${media.smDesktop`
+     margin-left:auto;
+     margin-right:auto;
+  `}
+  
   ${media.custom1`
-   margin-top: 20%;
+    margin-top: 20%;
   `}
 
   ${media.mdPhPort`
-   margin-top: 25%;
+    margin-top: 25%;
   `}
 `;
